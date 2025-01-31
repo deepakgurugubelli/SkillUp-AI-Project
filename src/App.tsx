11ee28b85ab5b55dfa8f8d31efd2a2f280c661df
deepakgurugubelli <gurugubellideepak54@gmail.com>
2025-01-31 12:04:10 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import CourseDetails from "./pages/CourseDetails";
+import Auth from "./pages/Auth";
+import Profile from "./pages/Profile";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/course/:courseId" element={<CourseDetails />} />
+          <Route path="/signup" element={<Auth />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
